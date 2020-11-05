8b24803c303b741bd855d1b97c2311403dadef78
Ravi Varshney <ravivarshney49@gmail.com>
2020-11-05 18:10:31 +0530

    bug fix and code formating

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import './Popup.css';
-import logo from '../../assets/img/meet.png'
+import logo from '../../assets/img/meet.png';
 
 const Popup = () => {
-  const [authUser, setAuthUser] = useState(0)
-  useEffect(()=> {
-    chrome.storage.local.get( ['user'], (res) => {
-      setAuthUser(res.user)
-    })
-  }, [])
+  const [authUser, setAuthUser] = useState(0);
+  useEffect(() => {
+    chrome.storage.local.get(['user'], (res) => {
+      if (res.user) {
+        setAuthUser(res.user);
+      }
+    });
+  }, []);
   const handleAuthUserChange = (e) => {
-    setAuthUser(e.target.value)
-    chrome.storage.local.set({user: e.target.value})
-  }
+    setAuthUser(e.target.value);
+    chrome.storage.local.set({ user: e.target.value });
+  };
 
   const createMeet = () => {
-    chrome.runtime.sendMessage({msg: 'create-meeting'})
-  }
+    chrome.runtime.sendMessage({ msg: 'create-meeting' });
+  };
   return (
     <div className="app">
       <div className="app__header">
@@ -25,10 +27,20 @@ const Popup = () => {
         <img src={logo} className="app__header__logo" alt="logo" />
       </div>
       <div className="app__form">
-        <label htmlFor="auth" className="app__form__label">Auth User</label>
-        <input id="auth" type="number" className="app__form__input" value={authUser}  onChange={handleAuthUserChange} />
+        <label htmlFor="auth" className="app__form__label">
+          Auth User
+        </label>
+        <input
+          id="auth"
+          type="number"
+          className="app__form__input"
+          value={authUser}
+          onChange={handleAuthUserChange}
+        />
       </div>
-      <button className="app__button" onClick={createMeet} >Create Meet</button>
+      <button className="app__button" onClick={createMeet}>
+        Create Meet
+      </button>
     </div>
   );
 };
